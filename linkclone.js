270405edbef2582b9368aa35481f0032286b9a48
axom websites <adamowaissi44@gmail.com>
2026-06-20 11:13:52 +0300

    Update linkclone.js

--- linkclone.js
+++ linkclone.js
@@ -6,10 +6,11 @@
       return;
     }
     if(!url.startsWith('http://') && !url.startsWith('https://')) url = 'https://' + url;
+    let method = document.getElementById('methodselect').value;
     window.app.showloading();
     let settings = window.settings ? window.settings.get() : { proxy: '', headers: {}, cookies: {} };
     try{
-      let html = await window.assetclone.clonefullsite(url, settings);
+      let html = await window.assetclone.clonefullsite(url, settings, method);
       window.app.setcurrenthtml(html, url);
     } catch(err){
       window.app.showerror('clone failed: ' + err.message);
